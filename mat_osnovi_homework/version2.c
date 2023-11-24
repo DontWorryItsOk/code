b37de54201a4bf8617120312044d1db05e7222a4
Styled0 <vladislavmikhailov2.0@mail.ru>
2023-11-24 15:12:34 +0300

    doing laba

--- mat_osnovi_homework/version2.c
+++ mat_osnovi_homework/version2.c
@@ -11,9 +11,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <stdlib.h>
 
 
-void dvoichn_in_des(char second[50], int *p_des) {
+int dvoichn_in_des(char second[50]) {
     int length = strlen(second);
     int des = 0, i = 0;
     for (i = 0; i < 50; i++) {
@@ -21,33 +22,47 @@ void dvoichn_in_des(char second[50], int *p_des) {
             des += 1 * pow(2, length - i - 1);
         }
     }
-    *p_des = des;  // Сохраняем значение des через указатель
+    return des;
 }
 
-void calculations(int first, int *p_des) {
-    int sum = first + *p_des;
-    int razn = first - *p_des;
-    int proizv = first * *p_des;
-    float delenie = (float)first / *p_des;
+void calculations(int first, int des) {
+    int sum = first + des;
+    int razn = first - des;
+    int proizv = first * des;
+    float delenie = (float)first / des;
 
     printf("\nСумма чисел равна %d ", sum);
     printf("\nРазность чисел равна %d ", razn);
     printf("\nПроизведение чисел равно %d ", proizv);
     printf("\nЧастное чисел равно %.2f", delenie);
+    // добавить перевод в двоичную систему
 }
 
 int main() {
-    int first, des;
+    int first;
     char second[50];
 
     printf("Введите 1-й операнд (число в 16-й сис-ме счисления): ");
     scanf("%x", &first);
+    
+    if(first > 65535) { // проверка вводимых значений - ограничение в 65535 - для 16-битовых чисел
+        printf("Вы ввели неправильное/слишком большое число");
+        exit(1);
+    }
 
     printf("Введите 2-й операнд (число в 2-й сис-ме счисления): ");
     scanf("%s", second);
 
-    dvoichn_in_des(second, &des);
-    calculations(first, &des);
+    for(int i = 0; i < (strlen(second)); i++) { // проверка вводимых значений
+    printf("%d", strlen(second));
+        if(second[i] != '0' && second[i] != '1') {
+            printf("Вы ввели число не в двоичной системе счисления");
+            exit(1);
+        }
+    }
+
+    int des = dvoichn_in_des(second);
+    calculations(first, des);
 
     return 0;
 }
